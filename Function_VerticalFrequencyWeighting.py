e823aff0cd72395958c232e12e9806739dc56a34
Josh Stevens <jste0058@student.monash.edu>
2025-05-30 16:56:51 +1000

    again

--- Function_VerticalFrequencyWeighting.py
+++ Function_VerticalFrequencyWeighting.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft, fftfreq
-from scipy.signal import freqs
 from scipy.fft import fft, ifft
 from tabulate import tabulate
 
