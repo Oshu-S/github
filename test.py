1d58dbc156bbe615cd2ff0931706e1d60fca5b1e
Josh Stevens <jste0058@student.monash.edu>
2025-09-03 15:02:29 +1000

    3/9/25

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-RMS_new = 0.027334
-RMS_0 = 0.025328
+RMS_0 = 0.140491
+RMS_new = 0.145176
 per_change = (RMS_new - RMS_0) / RMS_0 * 100
 print(per_change)
+
+E = 0.681899
+per_change_predicted = E*5
+print(per_change_predicted)
